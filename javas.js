01e82cc06a3662c8cd4f99358bc1b4a566a71df9
Nicolas Gimenez <33768042+foxrahn@users.noreply.github.com>
2020-03-04 11:24:41 -0300

    Version responsive 1

--- javas.js
+++ javas.js
@@ -1,33 +1,36 @@
-/*http://callmenick.com/post/expanding-search-bar-using-css-transitions*/
-(function($) {
-    "use strict";
-  
-    var $navbar = $(".nav"),
-        y_pos = $navbar.offset().top,
-        height = $navbar.height();
+// Sticky Header
+$(window).scroll(function() {
 
-    //scroll top 0 sticky
-    $(document).scroll(function() {
-        var scrollTop = $(this).scrollTop();
-        if (scrollTop > 0) {
-          $navbar.addClass("sticky");
-        } else {
-          $navbar.removeClass("sticky");  
-        }
-    });
-    
-    //section sticky
-    /*$(document).scroll(function() {
-        var scrollTop = $(this).scrollTop();
-        if ($(window).height() > scrollTop) {
-          $navbar.removeClass("sticky");
-        } else {
-          $navbar.addClass("sticky");  
-        }
-    });*/
+    if ($(window).scrollTop() > 100) {
+        $('.main_h').addClass('sticky');
+    } else {
+        $('.main_h').removeClass('sticky');
+    }
+});
 
-})(jQuery, undefined);
+// Mobile Navigation
+$('.mobile-toggle').click(function() {
+    if ($('.main_h').hasClass('open-nav')) {
+        $('.main_h').removeClass('open-nav');
+    } else {
+        $('.main_h').addClass('open-nav');
+    }
+});
 
-$(".menu").click(function(){
-  $("#nav").toggleClass("open");
+$('.main_h li a').click(function() {
+    if ($('.main_h').hasClass('open-nav')) {
+        $('.navigation').removeClass('open-nav');
+        $('.main_h').removeClass('open-nav');
+    }
+});
+
+// navigation scroll lijepo radi materem
+$('nav a').click(function(event) {
+    var id = $(this).attr("href");
+    var offset = 70;
+    var target = $(id).offset().top - offset;
+    $('html, body').animate({
+        scrollTop: target
+    }, 500);
+    event.preventDefault();
 });
